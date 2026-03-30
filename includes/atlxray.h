0533f0ba9bccd6c63dc5f50b3afae313aee70462
Mark Devenyi <markdevenyidev@gmail.com>
2026-03-30 15:46:02 -0400

    feat(kmod): add ATL debugfs interface

---
+++ includes/atlxray.h
@@ -0,0 +1,10 @@
+#ifndef ATLXRAY_H
+#define ATLXRAY_H
+
+#define ATL_MAGIC 0xAFA0
+
+struct atl_regs {
+    unsigned short magic;
+};
+
+#endif // ATLXRAY_H
--- src/kmod/main.c
+++ src/kmod/main.c
@@ -1,26 +1,70 @@
-// From: The Linux Kernel Module Programming Guide
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/err.h>
 
-/*
-* hello-2.c - Demonstrating the module_init() and module_exit() macros.
-* This is preferred over using init_module() and cleanup_module().
-*/
+#include <linux/printk.h>
+#include <linux/debugfs.h>
 
-#include <linux/init.h> /* Needed for the macros */
-#include <linux/module.h> /* Needed by all modules */
-#include <linux/printk.h> /* Needed for pr_info() */
+#include "atlxray.h"
 
-static int __init hello_2_init(void)
+static struct atl_regs regs = { 0 };
+static struct debugfs_blob_wrapper regs_wrapper = (struct debugfs_blob_wrapper) {
+    .data = NULL,
+    .size = sizeof(struct atl_regs)
+};
+
+static struct dentry *root = NULL;
+static struct dentry *regfile = NULL;
+
+static int __init init(void)
 {
-    pr_info("Hello, world 2\n");
+    pr_info("%s: loading...\n", KBUILD_MODNAME);
+
+    root = debugfs_create_dir("atlxray", NULL);
+    if (IS_ERR(root))
+    {
+        long ec = PTR_ERR(root);
+        pr_err("%s: debugfs_create_dir(): Fail with error %li!\n", KBUILD_MODNAME, ec);
+        return ec;
+    }
+
+    regs.magic = ATL_MAGIC;
+    /* TODO: Fill ATL register */
+
+    regs_wrapper.data = &regs;
+
+    regfile = debugfs_create_blob("regs", 0, root, &regs_wrapper);
+    if (IS_ERR(regfile))
+    {
+        long ec = PTR_ERR(regfile);
+        pr_err("%s: debugfs_create_blob(): Fail with error %li!\n", KBUILD_MODNAME, ec);
+        debugfs_remove(root);
+        return ec;
+    }
+
+    pr_info("%s: loaded!\n", KBUILD_MODNAME);
     return 0;
 }
 
-static void __exit hello_2_exit(void)
+static void __exit cleanup(void)
 {
-    pr_info("Goodbye, world 2\n");
+    pr_info("%s: unloading...\n", KBUILD_MODNAME);
+
+    if (root != NULL)
+    {
+        debugfs_remove(root);
+    }
+
+    if (regfile != NULL)
+    {
+        debugfs_remove(regfile);
+    }
+    pr_info("%s: unloaded!\n", KBUILD_MODNAME);
 }
 
-module_init(hello_2_init);
-module_exit(hello_2_exit);
+module_init(init);
+module_exit(cleanup);
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("arch.fail");
 MODULE_DESCRIPTION("Expose the registers of the ATL AMD64 interface");
+MODULE_VERSION("0.0.1");
